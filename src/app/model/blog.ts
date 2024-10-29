a3831fb13c8d26b6ba80861bf629027c4299cc85
David Schiavone <david.schiavone@hftm.ch>
2024-10-29 23:54:45 +0100

    feat: data definition of blogs

---
+++ src/app/model/blog.ts
@@ -0,0 +1,13 @@
+export interface Blog {
+  author: string;
+  comments: number;
+  contentPreview: string;
+  createdAt: string;
+  createdByMe: boolean;
+  headerImageUrl: string;
+  id: number;
+  likedByMe: boolean;
+  likes: number;
+  title: string;
+  updatedAt: string;
+}
