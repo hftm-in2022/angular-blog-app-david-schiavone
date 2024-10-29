f0cdee7a54e38bef1a4e5bd47658f813ef4e3575
David Schiavone <david.schiavone@hftm.ch>
2024-10-29 23:58:14 +0100

    feat: blog component

---
+++ src/app/blog-card/blog-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Blog } from '../model/blog';
+
+@Component({
+  selector: 'app-blog-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-card.component.html',
+  styleUrl: './blog-card.component.scss',
+})
+export class BlogCardComponent {
+  @Input()
+  blog: Blog | undefined;
+}
