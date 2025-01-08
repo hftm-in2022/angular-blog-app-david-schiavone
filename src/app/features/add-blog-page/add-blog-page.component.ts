35731e228a9730b76b2fc7d240410b355367bb8c
David Schiavone <david.schiavone@hftm.ch>
2025-01-09 00:41:09 +0100

    feat(add-blog): show spinner, go to overview after saving blog

--- src/app/features/add-blog-page/add-blog-page.component.ts
+++ src/app/features/add-blog-page/add-blog-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -8,26 +8,39 @@ import {
 import { BlogHeaderComponent } from '../../blog-header/blog-header.component';
 import { BlogService } from '../../services/blog.service';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { NgClass } from '@angular/common';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { AppState } from '../../services/redux/app-state';
+import { StateService } from '../../services/redux/state.service';
 
 @Component({
   selector: 'app-add-blog-page',
-  imports: [BlogHeaderComponent, ReactiveFormsModule, RouterLink, NgClass],
+  imports: [
+    BlogHeaderComponent,
+    ReactiveFormsModule,
+    RouterLink,
+    NgClass,
+    MatProgressSpinner,
+  ],
   templateUrl: './add-blog-page.component.html',
   styleUrl: './add-blog-page.component.scss',
 })
 export class AddBlogPageComponent {
-  blogForm: FormGroup;
   readonly titleMinLength = 3;
   readonly titleMaxLength = 20;
   readonly contentMinLength = 5;
   readonly contentMaxLength = 5000;
 
+  state: WritableSignal<AppState>;
+  blogForm: FormGroup;
+
   constructor(
     private formBuilder: FormBuilder,
     private blogService: BlogService,
     private oidcService: OidcSecurityService,
+    private stateService: StateService,
+    private router: Router,
   ) {
     this.blogForm = this.formBuilder.group({
       title: [
@@ -47,11 +60,18 @@ export class AddBlogPageComponent {
         ],
       ],
     });
+
+    this.state = this.stateService.getState();
   }
 
   submit() {
+    this.stateService.dispatch({ type: 'SET_LOADING', payload: true });
+
     this.oidcService.getAccessToken().subscribe((token) => {
-      this.blogService.addBlog(this.blogForm.value, token).subscribe();
+      this.blogService.addBlog(this.blogForm.value, token).subscribe(() => {
+        this.stateService.dispatch({ type: 'SET_LOADING', payload: true });
+        this.router.navigate(['/']);
+      });
     });
   }
 
